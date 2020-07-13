08514d71332c5e732a373dbabb2ab3306b811bde
usmanfawad <ufawad0@gmail.com>
2020-07-13 09:53:33 +0500

    root_window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+from tkinter.ttk import *
+from tkinter import *
+from tkinter import ttk
+
+
+class main_interface:
+    def __init__(self):
+        self.main_screen()
+
+
+    def main_screen(self):
+        self.root = Tk()
+        self.root.mainloop()
+
+
+x =main_interface()
